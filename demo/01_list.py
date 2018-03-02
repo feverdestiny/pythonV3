d922adf02e5271c1ce5ecf5dfef71169e5be8739
feverdestiny <feverdestiny@163.com>
2018-03-02 11:12:46 +0800

    添加列表、元组操作

---
+++ demo/01_list.py
@@ -0,0 +1,24 @@
+# List 列表
+
+a = ['1111', '22222', '33333', '444444', '1111']
+b = ('aaaa', 'bbbbbb', 'ccccc', '5555')
+
+# print(len(a))  # 列表长度
+
+# print(max(b)) #列表最大值
+# print(min(a)) #列表最小值
+# c = list(b)   #将元祖转换为列表
+# print(c)
+
+# a.append('text')  # 在列表末尾添加对象
+# print(a.count('1111'))  # 统计列表中某个元素出现的次数
+# a.extend(b)  # 在列表后追加多个新元素
+# a.insert(2, 'insert')  # 将对象插入到列表中
+# print(a.index('1111'))  # 列表中某个元素出现的位置
+# arr = a.pop(1)  # 移除列表中某个元素,并返回去除元素
+# a.remove('1111')  # 清除列表中匹配的第一个元素
+# a.clear()  # 清空列表
+# arr = a.copy()  # 复制列表
+# a.reverse()  # 列表反向
+# a.sort(Fun)  # 列表重新排序
+print(a)
---
+++ demo/02_tunple.py
@@ -0,0 +1,10 @@
+# 元组操作
+a = (1111, 22222, 33333, 44444, 555555)
+b = ['aaaa', 'bbbbb', 'ccccc']
+
+# print(len(a))  # 元组长度
+# print(max(a))  # 元组最大值
+# print(min(a))  # 元组最小值
+# print(a.index(33333))  # 元素出现的位置
+# print(a.count(1111))  # 元素出现的次数
+# c = tuple(b)  # 列表转换为元组
